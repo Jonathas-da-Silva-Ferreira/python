7536bc6d1b3262ad370e2a465c5b9456291e02d5
Jonathas da Silva Ferreira <105074747+Jonathas-da-Silva-Ferreira@users.noreply.github.com>
2025-01-21 10:26:12 -0300

    delet

--- software/main.py
+++
@@ -1,6 +0,0 @@
-from datetime import initialize_datetime
-
-if __name__ == '__main__':
-    # Initialize the datetime object
-    initialize_datetime()
-    print('Servidor Django inicializado com sucesso!')
