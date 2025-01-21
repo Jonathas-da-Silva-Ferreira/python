88f06191ba02db24452e497cce489551e2caeef1
Jonathas da Silva Ferreira <105074747+Jonathas-da-Silva-Ferreira@users.noreply.github.com>
2025-01-21 10:06:48 -0300

    novo

---
+++ software/main.py
@@ -0,0 +1,6 @@
+from datetime import initialize_datetime
+
+if __name__ == '__main__':
+    # Initialize the datetime object
+    initialize_datetime()
+    print('Servidor Django inicializado com sucesso!')
