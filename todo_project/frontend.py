c0d53caef23e513576bf89ad58a15a441c41aa5f
Jonathas da Silva Ferreira <105074747+Jonathas-da-Silva-Ferreira@users.noreply.github.com>
2025-01-29 11:20:02 -0300

    novo

---
+++ todo_project/frontend.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+import customtkinter as ctk
+import requests
+
+API_URL = "http://127.0.0.1:8000/api/tasks/"
+
+class TaskApp(ctk.CTK):
+    def __init__(self):
+        super().__init__()
+      
+        self.title("Gerenciador de Tarefas")
+        self.geometry("400x300")
+        
+        self.tasks_entry = ctk.CTKEntry(self, placeholder="Digite uma nova Tarefa")
+        self.tasks_entry.pack(pady=10)
+
+        self.add_button = ctk.CTKEntry(self, text="Adicionar Tarefa", comand=self.add_task)
+        self.add_button.pack()
+
+        self.tasks_listbox = tk.Listbox(self)
+        self.tasks_listbox.pack(pady=10)
+
+        self.get_tasks()
+
+    def get_tasks(self):
+        response = requests.get
--- todo_project/tasks/todo_project/settings.py
+++ todo_project/tasks/todo_project/settings.py
@@ -2,3 +2,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': 'rest_framework.permissions.IsAuthenticates',
     'PAGE_SIZE': 10
 }
+
+INSTALLED_APPS = ['corsheaders'] 
+MIDLLEWARE = ['corsheaders.middleware.CorsMiddleware']
+CORS_ALLOW_ALL_ORIGINS = True
